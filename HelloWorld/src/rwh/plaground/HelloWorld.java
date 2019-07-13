914d725fa08f83707e80dcd60e81f6f9c08dab3c
roywhunter <7939022+roywhunter@users.noreply.github.com>
2019-07-12 22:14:33 -0500

    updated package name
    Change-Id: I000000000000000000000000000000000000000

--- HelloWorld/src/rwh/plaground/HelloWorld.java
+++ HelloWorld/src/rwh/plaground/HelloWorld.java
@@ -1,8 +1,9 @@
+package rwh.plaground;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Well Roy It's Been a long time");
+		System.out.println("Well Roy It's Been a long time July 2019");
 	}
 }
